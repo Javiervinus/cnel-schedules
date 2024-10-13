a3bbf642dddcbdb33e9811b17b82dd598befa659
Javier Vinueza <javiervin@hotmail.com>
2024-10-13 12:42:27 -0500

    feat: Add Vercel serverless adapter
    Add Vercel serverless adapter with web analytics enabled to the output for better integration.

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,9 +4,14 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
+  output: "server",
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
   integrations: [
     tailwind({
       applyBaseStyles: false,
