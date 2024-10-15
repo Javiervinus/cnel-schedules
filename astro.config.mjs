bdbc2ce893f4697eaea5156a3958cdd0fc14abb0
Javier Vinueza <javiervin@hotmail.com>
2024-10-15 00:28:37 -0500

    feat: Update site output to static and add Open Graph & Twitter Card meta tags
    - Changed site output to static in the configuration
    - Added Open Graph & Twitter Card meta tags for improved social media sharing
    - Updated layout title on the index page to include additional information

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,12 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/serverless";
+import vercel from "@astrojs/vercel/static";
 
 // https://astro.build/config
 export default defineConfig({
-  output: "server",
+  site: "https://cnel-schedules.vercel.app",
+  output: "static",
   adapter: vercel({
     webAnalytics: { enabled: true },
   }),
