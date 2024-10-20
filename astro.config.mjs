97e56909fbad08cbbc701f3c78d8d03f538b2bd3
Javier Vinueza <javiervin@hotmail.com>
2024-10-19 20:34:49 -0500

    fix: Remove unused adapter configuration

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,17 +4,16 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
-import vercel from "@astrojs/vercel/static";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://cnel-schedules.vercel.app",
-  output: "static",
-  adapter: vercel({
-    webAnalytics: { enabled: true },
-  }),
+  // output: "static",
+  // adapter: vercel({
+  //   webAnalytics: { enabled: true },
+  // }),
   integrations: [
     tailwind({
       applyBaseStyles: false,
