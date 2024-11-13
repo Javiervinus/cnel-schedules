c5423abe0d73c1d7053cf53c9f8f3a97c7f75fe6
Javier Vinueza <javiervin@hotmail.com>
2024-11-13 06:46:08 -0500

    feat: add Contentful integration
    - Defined environment variables for Contentful.
    - Created a new module to initialize the Contentful client.

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,1 +1,11 @@
 /// <reference path="../.astro/types.d.ts" />
+
+interface ImportMetaEnv {
+  readonly CONTENTFUL_SPACE_ID: string;
+  readonly CONTENTFUL_DELIVERY_TOKEN: string;
+  readonly CONTENTFUL_PREVIEW_TOKEN: string;
+  readonly CONTENTFUL_ENVIRONMENT: string;
+  readonly GOOGLE_ADSENSE: string;
+  readonly GOOGLE_SITE_VERIFICATION: string;
+  readonly UMAMI_ID: string;
+}
